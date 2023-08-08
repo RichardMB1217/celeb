732386bd26813dca385b01ef69bbd73058dd0222
RichardMB1217 <116602303+RichardMB1217@users.noreply.github.com>
2023-08-07 23:48:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var loading = false;
 var after = '';
 var subreddit = '';
+var displayedImages = new Set();
+var firstImageUrls = { url1: '', url2: '' };
 
 function isInViewport(element) {
   var rect = element.getBoundingClientRect();
@@ -31,10 +33,9 @@ function shuffleSelectionTiles() {
 
 window.addEventListener('load', function() {
   shuffleSelectionTiles();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
   document.getElementById('imageResults').innerHTML = '';
 });
-
-var firstImageUrls = { url1: '', url2: '' };
 
 function loadPosts(subreddit, after = '') {
   if (!subreddit || loading) return;
@@ -42,7 +43,7 @@ function loadPosts(subreddit, after = '') {
 
   var url1 = `https://www.reddit.com/r/celebhub+celebnsfw/search.json?q=${subreddit}&restrict_sr=on&sort=top&include_over_18=true&after=${after}`;
   // var url2 = ''
-  var url2 = `https://www.reddit.com/r/${subreddit.replace(/\s/g, '')}.json?sort=top&include_over_18=true&after=${after}`;
+  var url2 = `https://www.reddit.com/r/${subreddit.replace(/[\s-]/g, '')}.json?sort=top&include_over_18=true&after=${after}`;
 
   fetch(url1)
     .then(function(response) {
@@ -83,7 +84,78 @@ function loadPosts(subreddit, after = '') {
     });
 }
 
-var displayedImages = new Set();
+// function processFetchedData(data) {
+//   var imageResults = document.getElementById('imageResults');
+
+//   after = data.data.after;
+
+//   var children = data.data.children;
+
+//   shuffleArray(children);
+
+//   children.forEach(function(child) {
+//     var mediaUrl = child.data.url_overridden_by_dest;
+//     var mediaType = child.data.post_hint;
+
+//     if ((mediaType === 'image' || mediaType === 'gif') && !displayedImages.has(mediaUrl)) {
+//       displayedImages.add(mediaUrl);
+//       var tempImage = new Image();
+//       tempImage.src = mediaUrl;
+//       tempImage.onload = function() {
+//         var imageWidth = tempImage.width;
+//         var imageHeight = tempImage.height;
+
+//         if (imageWidth > 180 && imageHeight > 100) {
+//           if (isInViewport(tempImage)) {
+//             var mediaContainer = document.createElement('div');
+//             mediaContainer.className = 'media';
+
+//             var media = document.createElement('img');
+//             media.src = mediaUrl;
+//             media.alt = child.data.title;
+//             media.style.maxHeight = '80vh';
+//             mediaContainer.appendChild(media);
+
+//             imageResults.appendChild(mediaContainer);
+//           }
+//         }
+//       };
+//     } else if (mediaType === 'gallery') {
+//       var galleryItems = child.data.gallery_data.items;
+
+//       galleryItems.forEach(function(galleryItem) {
+//         var mediaUrl = galleryItem.url;
+
+//         if (!displayedImages.has(mediaUrl)) {
+//           displayedImages.add(mediaUrl);
+//           var tempImage = new Image();
+//           tempImage.src = mediaUrl;
+//           tempImage.onload = function() {
+//             var imageWidth = tempImage.width;
+//             var imageHeight = tempImage.height;
+
+//             if (imageWidth > 180 && imageHeight > 100) {
+//               if (isInViewport(tempImage)) {
+//                 var mediaContainer = document.createElement('div');
+//                 mediaContainer.className = 'media';
+
+//                 var media = document.createElement('img');
+//                 media.src = mediaUrl;
+//                 media.alt = child.data.title;
+//                 media.style.maxHeight = '80vh';
+//                 mediaContainer.appendChild(media);
+
+//                 imageResults.appendChild(mediaContainer);
+//               }
+//             }
+//           };
+//         }
+//       });
+//     }
+//   });
+
+//   loading = false;
+// }
 
 function processFetchedData(data) {
   var imageResults = document.getElementById('imageResults');
@@ -95,13 +167,67 @@ function processFetchedData(data) {
   shuffleArray(children);
 
   children.forEach(function(child) {
-    var mediaUrl = child.data.url_overridden_by_dest;
-    var mediaType = child.data.post_hint;
+    if (child.data.is_gallery) {
+    Object.values(child.data.media_metadata).forEach(function(mediaItem) {
+      var mediaUrl = decodeURIComponent(mediaItem.s.u);
+    
+      if (!displayedImages.has(mediaUrl)) {
+        var manipulatedMediaUrl = mediaUrl.split('?')[0].replace('preview', 'i');
+        displayedImages.add(manipulatedMediaUrl);
+            
+        var tempImage = new Image();
+        tempImage.src = manipulatedMediaUrl;
+          tempImage.onload = function() {
+            var imageWidth = tempImage.width;
+            var imageHeight = tempImage.height;
 
-    if ((mediaType === 'image' || mediaType === 'gif') && !displayedImages.has(mediaUrl)) {
-      displayedImages.add(mediaUrl);
-      var tempImage = new Image();
-      tempImage.src = mediaUrl;
+            if (imageWidth > 180 && imageHeight > 100) {
+              if (isInViewport(tempImage)) {
+                var mediaContainer = document.createElement('div');
+                mediaContainer.className = 'media';
+
+                var mediaType;
+                switch (tempImage.src.split('.').pop()) {
+                  case 'gif':
+                    mediaType= 'gif';
+                    break;
+                  default:
+                    mediaType= 'image';
+                }
+
+                var mediaElement =
+                  (mediaType === 'image') ? 
+                  document.createElement('img') : 
+                  document.createElement('video');
+
+                if (mediaType === 'image') {
+                  mediaElement.src=manipulatedMediaUrl;
+                  }
+                 else{
+                   const source=document.createElement("source");
+                   source.type="video/mp4";
+                   source.src=tempImage.src.replace(/&amp;/g, '&');
+                   mediaElement.appendChild(source);
+                 }
+                  
+                mediaElement.alt = child.data.title;
+                mediaElement.style.maxHeight = '80vh';
+                mediaContainer.appendChild(mediaElement);
+
+                imageResults.appendChild(mediaContainer);
+              }
+            }
+          };
+        }
+      });
+    } else {
+      var mediaUrl = child.data.url_overridden_by_dest;
+      var mediaType = child.data.post_hint;
+
+      if ((mediaType === 'image' || mediaType === 'gif') && !displayedImages.has(mediaUrl)) {
+        displayedImages.add(mediaUrl);
+        var tempImage = new Image();
+        tempImage.src = mediaUrl;
       tempImage.onload = function() {
         var imageWidth = tempImage.width;
         var imageHeight = tempImage.height;
@@ -120,13 +246,13 @@ function processFetchedData(data) {
             imageResults.appendChild(mediaContainer);
           }
         }
+      }
       };
     }
   });
 
   loading = false;
 }
-
 
 window.addEventListener('scroll', function() {
   var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
@@ -153,4 +279,6 @@ function showCelebrityImages(celebrityName, celebrityNameElement) {
   imageResults.innerHTML = '';
   document.getElementById('celebGrid').style.display = 'none';
   loadPosts(subreddit);
+  imageResults.innerHTML = '';
+  document.getElementById('imageResults').style.display = 'flex';
 }
